d6dbfc46bb96ced7641075d90142cf643d2bec7b
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-12 14:33:55 +0700

    in the process

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution s = new Solution();
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.add(2);
+        l1.add(4);
+        l1.add(3);
+        System.out.println(l1.);
+
     }
 }
